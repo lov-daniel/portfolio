ce57edd3cf1aa90828e0ae78c11305195b2301fd
unuphan <lovdaniel11@gmail.com>
2026-09-25 00:28:28 -0700

    fleshed out project page and contact me

--- src/constants/projectList.jsx
+++ src/constants/projectList.jsx
@@ -3,7 +3,7 @@ const projects = {
         name: "Triton Planning",
         description: "lorem ipsem",
         repo: "https://github.com/lov-daniel/triton-planning",
-        tags: ["UTILITY, SCHOOL, FEATURED"],
+        tags: ["UTILITY", "SCHOOL", "FEATURED"],
         id: 1
     },
 
@@ -19,14 +19,48 @@ const projects = {
         name: "T.I.A.N.",
         description: "lorem ipsem 3",
         repo: "https://github.com/lov-daniel/tian",
-        tags: ["GAMES"]
+        tags: ["GAMES"],
+        id: 3
     },
 
     "project4": {
         name: "Socket Surge",
         description: "lorem ipsem 4",
         repo: "https://github.com/yyunshanli/Socket-Surge-Chess",
-        tags: ["GAMES", "SCHOOL", "UTILITY"]
+        tags: ["GAMES", "SCHOOL"],
+        id: 4
+    },
+
+    "project5": {
+        name: "Pibby Says",
+        description: "lorem ipsem 4",
+        repo: "https://github.com/yyunshanli/Socket-Surge-Chess",
+        tags: ["GAMES", "SCHOOL", "ELECTRONICS", "FEATURED"],
+        id: 5
+    },
+
+    "project6": {
+        name: "StudyBox",
+        description: "lorem ipsem 4",
+        repo: "https://github.com/lov-daniel/diamond-hacks",
+        tags: ["SCHOOL", "UTILITY"],
+        id: 6
+    },
+
+    "project7": {
+        name: "Flood Gate",
+        description: "lorem ipsem 4",
+        repo: "https://github.com/lov-daniel/floodgate",
+        tags: ["UTILITY"],
+        id: 7
+    },
+
+    "project8": {
+        name: "Trading Agent Orchestrator",
+        description: "lorem ipsem 4",
+        repo: "https://github.com/lov-daniel/floodgate",
+        tags: ["UTILITY", "FEATURED", "WORK IN PROGRESS"],
+        id: 6
     },
 }
 
